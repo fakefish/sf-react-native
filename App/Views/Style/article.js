b6a92a2504f5fe276b9fc7f310bc649a245c8d6f
王佳裕 <fakefish.wjy@gmail.com>
2015-04-15 23:40:33 +0800

    修复一些错误

---
+++ App/Views/Style/article.js
@@ -0,0 +1,14 @@
+
+var React = require('react-native');
+var {
+  StyleSheet
+} = React;
+module.exports = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+    backgroundColor: '#fff',
+  },
+});
